25df121a2cd567a1a4cf55e152153812df8b2b8d
yvaldez087 <yvaldez087@gmail.com>
2024-10-07 21:52:38 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsmember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/skills-member.html'
+  };
+}
+
+function test () {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/test.html'
+  };
+}
+    }
