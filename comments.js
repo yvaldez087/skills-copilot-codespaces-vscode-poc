1e06470fa74fc8fe731975753bdab0bc198edac3
yvaldez087 <yvaldez087@gmail.com>
2024-10-07 22:28:38 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// create web server
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    console.log('Request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+});
